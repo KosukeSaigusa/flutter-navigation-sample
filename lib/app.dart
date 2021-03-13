fda863fc527b1989d91a25a79ef15c899abb4c36
kosukesaigusa <saigusa758cloudy@gmail.com>
2021-03-13 13:10:45 +0900

    feat(app.dart): implemented application root widget

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation_sample/screens/home/home_screen.dart';
+
+class NavigationSampleApp extends StatelessWidget {
+  final String appTitle = 'Flutter Navigation Sample';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation Sample',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(title: appTitle),
+    );
+  }
+}
